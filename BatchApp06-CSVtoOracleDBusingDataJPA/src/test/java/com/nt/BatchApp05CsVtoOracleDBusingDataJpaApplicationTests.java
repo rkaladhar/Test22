21dc37d2fb6cacefeb1f3ffc7a623d2dc7d6b7a6
J Ravi Kalyana Kaladhar <rkaladhar@gmail.com>
2023-10-07 23:49:18 +0530

    commit

--- BatchApp06-CSVtoOracleDBusingDataJPA/src/test/java/com/nt/BatchApp05CsVtoOracleDBusingDataJpaApplicationTests.java
+++ BatchApp06-CSVtoOracleDBusingDataJPA/src/test/java/com/nt/BatchApp05CsVtoOracleDBusingDataJpaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootDataJpaProj01CrudRepositoryApplicationTests {
+class BatchApp05CsVtoOracleDBusingDataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
